787f7245e91cedf14c42f57d273f8e4f2b233ad8
n-kolin <1020nomi@gmail.com>
2025-02-09 02:07:16 +0200

    await to node

--- node/index.js
+++ node/index.js
@@ -1,15 +1,12 @@
-import express from 'express';
-import renderApi from '@api/render-api';
-
-// const express = require("express");
+const express = require("express");
 const app = express();
 const port = 3000;
-// const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j')
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j')
  
 
 app.get("/", async (req, res) => {
-    renderApi.auth(process.env.API_KEY)
-    await renderApi.getServices({limit:'20'})
+    sdk.auth(process.env.API_KEY)
+    await sdk.getServices({limit:'20'})
     .then(({data})=>res.json(data))
     .catch(e=>console.log(e))
 });
